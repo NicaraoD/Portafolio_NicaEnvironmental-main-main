279bdbd4a911a45ee2a8b59cc6762e39fcd16887
NicaraoD <nicaraodelgado@gmail.com>
2025-12-10 21:32:27 -0600

    create build

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useForm } from '@formspree/react';
 import PageLayout from '../components/PageLayout';
 import Footer from '../components/Footer';
